c841446af85d8e0d065036b5f0df74c162b6fc38
K UDAY KUMAR <72476990+UDAY368@users.noreply.github.com>
2022-03-22 11:13:53 +0530

    Basic working with the urls and views. Video ref: 6

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -1,1 +1,11 @@
 # This file is created by Uday
+# This module always return the responses
+from django.http import HttpResponse
+
+
+def index(request):       # It must take the at least one argument
+    return HttpResponse("Hello World")
+
+
+def about(request):
+    return HttpResponse("This page is about Django Usage")
